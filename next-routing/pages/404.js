40adecee37a0bbaa18f3bf10cadced553483c668
tribagus <tri.bagus21@gmail.com>
2022-10-31 21:49:07 +0700

    add 404 page

---
+++ next-routing/pages/404.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>PageNotFound</h1>
+      <Link href="/">Go back to Home</Link>
+    </div>
+  )
+}
+
+export default PageNotFound
--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,12 +1,18 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Home() {
+  const router = useRouter()
+
   return (
     <div>
       <h1>Home Page</h1>
       <Link href="/blog">Go to Blog</Link>
       <br />
       <Link href="/product">Go to Products</Link>
+      <br />
+      <br />
+      <button onClick={() => router.push("/product")}>Place Order</button>
     </div>
   )
 }
