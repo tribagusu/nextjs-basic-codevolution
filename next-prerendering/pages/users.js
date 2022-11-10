a8c1420934517b400de6b32e714fd3af55ec8657
tribagus <tri.bagus21@gmail.com>
2022-11-10 11:32:00 +0700

    get static props

---
+++ next-prerendering/pages/users.js
@@ -0,0 +1,31 @@
+function UserList({ users }) {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>
+              {user.name}
+              {user.email}
+            </p>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default UserList
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await response.json()
+  console.log(data)
+
+  return {
+    props: {
+      users: data,
+    },
+  }
+}
